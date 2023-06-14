2e757a3bcd9790cb251accb117d6c7d7c25b696f
umar hussain <bsf2001016@ue.edu.pk>
2023-06-14 05:41:45 -0700

    commit1

---
+++ travelling-blog/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+
+import Navbar from './navbar';
+import Home from './home';
+
+function App() {
+ 
+  return (
+   
+    <div className="App">
+   <Navbar />
+   <Home />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ travelling-blog/src/navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1> Travelling Blog </h1>
+            <div className="link">
+                <a href="/home">Home</a>
+                
+                <a href="/create">Create</a>
+                
+                <a href="/contact us">contact us</a>
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
